c37d2cbf99465a070c287b50da95003ad24720b0
GKNIGHT <113472704+gerexompad1@users.noreply.github.com>
2022-12-19 01:12:54 +0800

    categories and cross origin

--- elbmpe/src/main/java/com/mathopia/elbmpe/Controller/CategoriesController.java
+++ elbmpe/src/main/java/com/mathopia/elbmpe/Controller/CategoriesController.java
@@ -3,6 +3,7 @@ package com.mathopia.elbmpe.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.mathopia.elbmpe.Service.CategoriesService;
 
 @RestController
 @RequestMapping("/categories")
+@CrossOrigin
 public class CategoriesController {
 	@Autowired
 	CategoriesService cserv;
